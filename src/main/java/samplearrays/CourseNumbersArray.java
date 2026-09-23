59234c7dc209a742532d363c4c367b7ab3e734bd
Mohammed Boukham <mohammedboukhammail@gmail.com>
2026-09-23 17:31:58 +0000

    finished exercise 1

--- src/main/java/samplearrays/CourseNumbersArray.java
+++ src/main/java/samplearrays/CourseNumbersArray.java
@@ -5,4 +5,22 @@ public class CourseNumbersArray {
         int[] registeredCourses = {1010, 1020, 2080, 2140, 2150, 2160};
 
     }
+    public static int[] updatedCourses(int[] registeredCourses, int new_course){
+        int n=registeredCourses.length;
+        int[] registeredCoursesUpdated = new int[n+1];
+        for (int i=0;i<n;i++){
+            registeredCoursesUpdated[i]=registeredCourses[i];
+        }
+        registeredCoursesUpdated[n]=new_course;
+        System.out.println(registeredCoursesUpdated);
+        return registeredCoursesUpdated;
+    } 
+    public static boolean checksRegisteredCourses(int[] registeredCourses, int course){
+        int n=registeredCourses.length;
+        for(i=0;i<n;i++){
+            if(registeredCourses[i]==course){return true;} 
+        }
+        return false;
+    }
+
 }
